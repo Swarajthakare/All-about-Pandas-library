bb7c581397c4da35de76f8e3a1929aac19477ff5
SWARAJ THAKARE <137782161+Swarajthakare@users.noreply.github.com>
2025-03-18 23:38:19 +0530

    Add files via upload

---
+++ 20 Pivot Table.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## what is Pandas Pivot Table?
+# 
+# #### The levels in the pivot table will be stored in multiindex objects (hierarchical indexes) on the index and columns of the result DataFrame.
+# 
+# 
+# ### syntex : DaraFrame.Pivot_Table()
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[ ]:
+
+
+
+
